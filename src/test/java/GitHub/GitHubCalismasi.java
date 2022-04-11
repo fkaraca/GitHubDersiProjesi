2e1965ac84eefd14c5841f4aea1aa934c615f43b
fkaraca <fatmakaraca19@outlook.com>
2022-04-11 00:21:18 -0400

    toplama islemi

--- src/test/java/GitHub/GitHubCalismasi.java
+++ src/test/java/GitHub/GitHubCalismasi.java
@@ -5,6 +5,9 @@ public class GitHubCalismasi {
     public static void main(String[] args) {
         System.out.println("GitHub Dersi Basladi!");
         System.out.println("Github push gerceklesti");
+        int a=5;
+        int b=8;
+        System.out.println(a+b);
 
     }
 }
