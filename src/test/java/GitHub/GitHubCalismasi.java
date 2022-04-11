b889e91f52c5c2de68aaefe9ead80e8c3765e415
fkaraca <fatmakaraca19@outlook.com>
2022-04-11 00:09:53 -0400

    ikinci print satiri eklendi

--- src/test/java/GitHub/GitHubCalismasi.java
+++ src/test/java/GitHub/GitHubCalismasi.java
@@ -4,6 +4,7 @@ public class GitHubCalismasi {
 
     public static void main(String[] args) {
         System.out.println("GitHub Dersi Basladi!");
+        System.out.println("Github push gerceklesti");
 
     }
 }
